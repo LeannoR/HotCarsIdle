4e273dd41d1baddf267e5d7f12de2d9e8c0dfe21
Onur <onur.durgun@hotmail.com>
2022-12-15 11:15:17 +0300

    DisplayCreateCarCost, DisplayMergeCarCost and DisplayDolar added.

--- Hot Cars Idle/Assets/Scripts/GameManager.cs
+++ Hot Cars Idle/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,20 +10,26 @@ public class GameManager : MonoBehaviour
     public int dolars = 0;
     public int mergeCarCost = 10;
     public int createCarCost = 5;
+    public TextMeshProUGUI dolarCountText;
+    public TextMeshProUGUI createCarCostText;
+    public TextMeshProUGUI mergeCarCostText;
     public List<GameObject> carLevelList = new List<GameObject>();
 
     public void Update()
     {
+        DisplayDolar();
+        DisplayCreateCarCost();
+        DisplayMergeCarCost();
         CarSpeedBoost();
     }
     public void CarSpeedBoost()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             timeRemaining = 1f;
             Time.timeScale = 2f;
         }
-        if(timeRemaining <= 0)
+        if (timeRemaining <= 0)
         {
             Time.timeScale = 1f;
         }
@@ -30,7 +38,6 @@ public class GameManager : MonoBehaviour
     public void AddingMoneyFromCheckPoint(int income)
     {
         dolars += income;
-        Debug.Log(dolars);
     }
     public void MergeSalary()
     {
@@ -55,7 +62,7 @@ public class GameManager : MonoBehaviour
     }
     public bool CanWeAffordMergeSalary()
     {
-        if(dolars >= mergeCarCost)
+        if (dolars >= mergeCarCost)
         {
             return true;
         }
@@ -63,6 +70,19 @@ public class GameManager : MonoBehaviour
         {
             return false;
         }
+    }
+
+    public void DisplayDolar()
+    {
+        dolarCountText.text = dolars.ToString();
+    }
+    public void DisplayCreateCarCost()
+    {
+        createCarCostText.text = createCarCost.ToString() + "\nCreate Car";
+    }
+    public void DisplayMergeCarCost()
+    {
+        mergeCarCostText.text = mergeCarCost.ToString() + "\nMerge Car";
     }
 }
 
